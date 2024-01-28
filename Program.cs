e4b2bb5a33770aba1410cac6c0354a66224ac563
huynhtuankiet123 <91444536+huynhtuankiet123@users.noreply.github.com>
2024-01-28 11:12:16 +0700

    Add files via upload
    bài tập về nhà LTHDT

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Baitapvidu
+{
+     class Program
+    {
+        static void NhapDay(int n, int[] A)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Nhap A[{i}]= ");
+                A[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static int TongDuong(int n,int[] A)
+        {
+            int tong = 0;
+            for(int i= 0;i < n;i++)
+            {
+                if (A[i] > 0)
+                {
+                    tong+=A[i];
+                }                   
+            }
+            return tong;
+        }
+    static void Main(string[] args)
+        {
+            int n;
+            do
+            {
+                Console.Write("Nhap n phan tu cua day so: ");
+                n = int.Parse(Console.ReadLine());
+            } while (2 >= n || n >= 20);
+            int[] A = new int[n];
+            NhapDay(n,A);
+            int kq = TongDuong(n,A);
+            Console.Write($"Tong cac so nguyen duong trong day = {kq}");
+            Console.ReadKey();
+        }
+    }
+}
